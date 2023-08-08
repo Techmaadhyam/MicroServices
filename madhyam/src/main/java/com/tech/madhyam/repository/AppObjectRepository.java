e62b6ecc2e4bfd0672ffec5ee7f1b19d9a6c1b4f
Techmaadhyam <123808278+Techmaadhyam@users.noreply.github.com>
2023-08-08 13:01:37 +0530

    Delete madhyam/src/main/java/com/tech/madhyam/repository directory

--- madhyam/src/main/java/com/tech/madhyam/repository/AppObjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tech.madhyam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tech.madhyam.entity.AppObject;
-
-public interface AppObjectRepository extends JpaRepository<AppObject, Long>{
-    
-}
--- madhyam/src/main/java/com/tech/madhyam/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tech.madhyam.repository;
-
-import com.tech.madhyam.entity.Company;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long>{
-}
